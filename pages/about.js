eff8b76348c1095d55d7b194bc724b80c8d9b2bd
Jorge Romussi <jromussi@gmail.com>
2019-11-19 10:12:09 -0300

    avancee ghpages

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout.js';
+
+const aboutPageContent = <p>This is the about page</p>;
+
+export default function About() {
+  return <Layout content={aboutPageContent} />;
+}
