563aa4073c0ef7904ba8feb9ab65376c50d41023
Jorge Romussi <jromussi@gmail.com>
2019-11-19 10:18:51 -0300

    avance

--- components/Header.js
+++ components/Header.js
@@ -8,10 +8,10 @@ const linkStyle = {
 
 const Header = () => (
   <div>
-    <Link href="/">
+    <Link href="/" as={ process.env.BACKEND_URL + '/'}>
       <a style={linkStyle}>Home</a>
     </Link>
-    <Link href="/about">
+    <Link href="/about" as={ process.env.BACKEND_URL + '/about'}>
       <a style={linkStyle}>About</a>
     </Link>
   </div>
